2c57ada3eb3f2108be0063cab32e022f6b1557ea
ttzztztz <935319340@qq.com>
2019-07-01 08:55:24 +0800

    Fix dependencies

--- model/storage.js
+++ model/storage.js
@@ -1,46 +1,42 @@
-const sqlite = require("sqlite");
-const SQL = require("sql-template-strings");
+const SQLite = require("sql.js");
+const fs = require("fs");
 
 class Storage {
-  static connectDB = async () => {
-    const db = await sqlite.open("./database.sqlite");
-    await db.all(
-      SQL`CREATE TABLE IF NOT EXISTS list(time INT(10), title CHAR(50), description TEXT, status INT(10), due INT(10))`
+  constructor(){
+    const buffer = fs.readFileSync("./database.sqlite");
+    const db = new SQLite(buffer);
+    db.exec(
+      `CREATE TABLE IF NOT EXISTS list(time INT(10), title CHAR(50), description TEXT, status INT(10), due INT(10))`
     );
-    return db;
+    this.db = db;
   };
 
-  static existListItem = async title => {
-    const db = await this.connectDB();
-    const resultArr =  await db.all(
+  existListItem = title => {
+    const resultArr =  this.db.exec(
       `SELECT * FROM list WHERE title = ?`,
       [title]
     );
     return resultArr && resultArr.length >= 1;
   };
 
-  static addListItem = async (title, description, status = 1, due) => {
-    const db = await this.connectDB();
+  addListItem = (title, description, status = 1, due) => {
     const nowTimestamp = (new Date().getTime() / 1000) | 0;
-    return await db.all(
+    return this.db.exec(
       `INSERT INTO list(time, title, description, status, due) VALUES (?, ?, ?, ?, ?)`,
       [nowTimestamp, title, description, status, due]
     );
   };
 
-  static deleteListItem = async title => {
-    const db = await this.connectDB();
-    return await db.all(`DELETE FROM list WHERE title = ?`, [title]);
+   deleteListItem =  title => {
+    return this.db.exec(`DELETE FROM list WHERE title = ?`, [title]);
   };
 
-  static updateListItem = async (title, status) => {
-    const db = await this.connectDB();
-    return await db.all(`UPDATE list SET (status = ?) WHERE (title = ?)`, [Number.parseInt(status), title]);
+  updateListItem =  (title, status) => {
+    return this.db.exec(`UPDATE list SET (status = ?) WHERE (title = ?)`, [Number.parseInt(status), title]);
   };
 
-  static allListItem = async () => {
-    const db = await this.connectDB();
-    const result = await db.all(`SELECT * FROM list ORDER BY due DESC`);
+  allListItem = () => {
+    const result = this.db.exec(`SELECT * FROM list ORDER BY due DESC`);
     return result;
   };
 }
