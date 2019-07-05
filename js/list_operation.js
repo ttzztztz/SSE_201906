39252e84d42750c2c5da43963d815ea5b8b2987a
Anastasiawangyx <43713795+Anastasiawangyx@users.noreply.github.com>
2019-07-05 09:46:21 +0800

    test

--- js/list_operation.js
+++ js/list_operation.js
@@ -17,3 +17,15 @@ const abortTask = title => {
     status: 3
   });
 };
+
+const generateWords = ()=>{
+  var str=["我们最大的弱点在于放弃。成功的必然之路就是不断的重来一次。",
+  "忍耐和坚持虽是痛苦的事情，但却能渐渐地为你带来好处。",
+  "一个人只要强烈地坚持不懈地追求，他就能达到目的。",
+  "我们最大的弱点在于放弃。成功的必然之路就是不断的重来一次。",
+  "我可以接受失败，但我不能接受放弃!"];
+  
+  var ran=Math.floor(Math.random()*5);
+  const DOM = document.querySelector("#encourageWords");
+  DOM.innerHTML=str[ran];
+}
